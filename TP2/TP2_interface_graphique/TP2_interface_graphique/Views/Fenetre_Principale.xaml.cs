5eff157b004055149887b82acdd9ba43af0c7627
Frederik Boutin <Frederik.Boutin@uqar.ca>
2022-04-07 17:49:40 -0400

    Deuxieme fenetre de completee

---
+++ TP2/TP2_interface_graphique/TP2_interface_graphique/Views/Fenetre_Principale.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP2_interface_graphique.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Fenetre_Principale.xaml
+    /// </summary>
+    public partial class Fenetre_Principale : Window
+    {
+        public Fenetre_Principale()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TP2/TP2_interface_graphique/TP2_interface_graphique/Views/Window_New_user.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP2_interface_graphique.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Window_New_user.xaml
+    /// </summary>
+    public partial class Window_New_user : Window
+    {
+        public Window_New_user()
+        {
+            InitializeComponent();
+        }
+    }
+}
